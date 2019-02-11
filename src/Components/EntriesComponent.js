06ab60b0164e2c664b22c823f766c14aba937398
jkhaha <jha2@wellesley.edu>
2019-02-10 21:41:36 -0500

    added buttons

--- src/Components/EntriesComponent.js
+++ src/Components/EntriesComponent.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import { loadEntries } from '../store/actions/entryActions'
 
 class EntriesComponent extends Component {
+  handleClick = (e) => {
+    console.log("hi i'm here", this.props);
+  }
+
   render(){
+    console.log(this.props);
     return(
       <div>
-      <div id="ui_checkbox">
-        <input type="checkbox" id="my_check" name="example"></input> <label>{this.props.entry.id}</label>
+      <div>
+        <span>
+          <p className="entries_id">{this.props.entry.id}</p>
+          <button onClick={this.handleClick} className="entries_button">Completed</button>
+        </span>
       </div>
     </div>
     )
@@ -19,10 +28,10 @@ const mapStateToProps = (state) => {
   }
 }
 
-// const mapDispatchToProps = (dispatch) => {
-//   return {
-//     loadEntries: () => dispatch(loadEntries())
-//   }
-// }
+const mapDispatchToProps = (dispatch) => {
+  return {
+    loadEntries: () => dispatch(loadEntries())
+  }
+}
 
 export default connect(mapStateToProps)(EntriesComponent)
